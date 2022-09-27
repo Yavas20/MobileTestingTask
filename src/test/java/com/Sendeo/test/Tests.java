c264a05388baf2311066db3624689ccf082c1959
Yavas20 <emreyavas20@hotmail.com>
2022-09-27 09:51:56 +0300

    final commit

--- src/test/java/com/Sendeo/test/Tests.java
+++ src/test/java/com/Sendeo/test/Tests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.Sendeo.utils.Driver;
 import com.Sendeo.utils.ExcelUtil;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
